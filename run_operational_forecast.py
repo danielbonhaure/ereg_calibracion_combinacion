33e33502a6888f0684e61793fae78b9077e4c372
Daniel Bonhaure <danielbonhaure@gmail.com>
2024-09-14 16:08:43 -0400

    improved: run_operational_forecast.py arguments

--- run_operational_forecast.py
+++ run_operational_forecast.py
@@ -112,14 +112,14 @@ def main(args):
                     get_mme_parameters(argparse.Namespace(variable=[v], IC=[args.month], leadtime=[l], 
                                                           OW=args.overwrite, no_models=[], wtech=[w]))
 
-    if args.combine or args.combine_main:
+    if args.combine and args.combine_main:
         cfg.logger.info("Starting combination")
         for v in args.variables:  # loop sobre las variables a calibrar
             for l in range(1, 7+1):  # loop over leadtime --> Forecast leadtime (in months, from 1 to 7)
                 for c, w in itertools.product(args.combination, args.weighting): 
                     real_time_combination(argparse.Namespace(variable=[v], IC=[f"{args.year}-{args.month}-01"], 
                                                              leadtime=[l], no_models=[], ctech=c, wtech=[w]))
-    if args.combine or args.combine_sissa:
+    if args.combine and args.combine_sissa:
         cfg.logger.info("Starting combination SISSA")
         for v in args.variables:  # loop sobre las variables a calibrar
             for l in range(1, 7+1):  # loop over leadtime --> Forecast leadtime (in months, from 1 to 7)
@@ -127,13 +127,13 @@ def main(args):
                     real_time_combination_sissa(argparse.Namespace(variable=[v], IC=[f"{args.year}-{args.month}-01"],
                                                                    leadtime=[l], no_models=[], ctech=c, wtech=[w]))
   
-    if args.plot or args.plot_main:
+    if args.plot and args.plot_main:
         cfg.logger.info("Starting plotting")
         for v in args.variables:  # loop sobre las variables a calibrar
             for l in range(1, 7+1):  # loop over leadtime --> Forecast leadtime (in months, from 1 to 7)
                 plot_rt_forecast(argparse.Namespace(variable=[v], IC=[f"{args.year}-{args.month}-01"], leadtime=[l],
                                                     weighting=args.weighting, combination=args.combination))
-    if args.plot or args.plot_sissa:
+    if args.plot and args.plot_sissa:
         cfg.logger.info("Starting plotting SISSA")
         for v in args.variables:  # loop sobre las variables a calibrar
             for l in range(1, 7+1):  # loop over leadtime --> Forecast leadtime (in months, from 1 to 7)
